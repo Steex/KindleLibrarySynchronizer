314d9424f5a4471e8805dc2d50af90ff4e427670
Steex2 <sot@tut.by>
2015-07-07 22:49:57 +0300

    LibraryInfo class introduced to store data about libraries.

---
+++ Sources/LibraryInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace KindleLibrarySynchronizer
+{
+	public class LibraryInfo
+	{
+		public string Name { get; set; }
+		public string SourceRoot { get; set; }
+		public string TargetRoot { get; set; }
+		public List<string> SkippedFiles { get; private set; }
+
+		public LibraryInfo()
+		{
+			SkippedFiles = new List<string>();
+		}
+	}
+
+}
